ce0fe8ed12be1d700a6a38c555a1d1288e5953e4
Sonali Kishan <97959454+sonalikishan@users.noreply.github.com>
2024-06-04 01:03:51 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -11,11 +11,11 @@ export const dialogueData = {
   tv: `That's my TV. I've been watching tech YouTubers a lot recently like :
    <a href="https://www.youtube.com/@BroCodez" target="_blank">Bro Code</a>, <a href="https://www.youtube.com/@freecodecamp" target="_blank">Free Code Camp</a>,
   <a href="https://www.youtube.com/@thesheryianscodingschool" target="_blank">Sheryians Coding School</a> (sometimes) and <a href="https://www.youtube.com/@6PackProgrammer" target="_blank">6 Pack Programmer</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it. Nowadays I sleep a lot(Zzzzzz). Looks like I have to Coffee on a daily HEHE`,
-  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/drive/folders/1kBxDD7sksn-bVFvUDy6N9yelCWwbTMmR?usp=sharing" target="_blank">Drive Link To My Resume</a>
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it. Nowadays I sleep a lot(Zzzzzz). Looks like I have to chug Coffee on a daily HEHE`,
+  resume: `This is my desk and on it is my resume. <a href="https://pdf.ac/3ESUvL" target="_blank">Link To My Resume</a>
   Contact me at 'sonalikn75@gmail.com' if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.,
-  library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit Sonali's portfolio, just close the tab.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in French. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
 };
