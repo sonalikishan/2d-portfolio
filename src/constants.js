6ab44bd7178fbd2e9ed8ae1824866c66ca3eacef
Sonali Kishan <97959454+sonalikishan@users.noreply.github.com>
2024-06-03 23:54:54 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,22 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  pc: `This is my PC. I work mostly in JavaScript, Reactjs, and Nodejs these days.
+    I've made a couple of games in that language. I also like C++ and Java. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/sonalikishan" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Computer Engineering degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  I love to dance and write. Mostly, I love to write in my secret notes app. However, I have started writing down my thoughts in Wordpress. You can connect with me through instagram. <a href="https://www.instagram.com/heysonalii/" target="_blank">Instagram</a>! 
+  hehe!`,
+  tv: `That's my TV. I've been watching tech YouTubers a lot recently like :
+   <a href="https://www.youtube.com/@BroCodez" target="_blank">Bro Code</a>, <a href="https://www.youtube.com/@freecodecamp" target="_blank">Free Code Camp</a>,
+  <a href="https://www.youtube.com/@thesheryianscodingschool" target="_blank">Sheryians Coding School</a> (sometimes) and <a href="https://www.youtube.com/@6PackProgrammer" target="_blank">6 Pack Programmer</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it. Nowadays I sleep a lot(Zzzzzz). Looks like I have to Coffee on a daily HEHE`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/drive/folders/1kBxDD7sksn-bVFvUDy6N9yelCWwbTMmR?usp=sharing" target="_blank">Drive Link To My Resume</a>
+  Contact me at 'sonalikn75@gmail.com' if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.,
+  library: `There are a lot of programming books on my shelves.
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit Sonali's portfolio, just close the tab.`,
 };
