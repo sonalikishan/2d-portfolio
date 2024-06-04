96e68d9d11a47f28a7479621f506465d6237327d
Sonali Kishan <97959454+sonalikishan@users.noreply.github.com>
2024-06-04 13:27:42 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -17,5 +17,5 @@ export const dialogueData = {
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
   library: `There are a lot of programming books on my shelves. There is even one in French. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit Sonali's portfolio, just close the tab.`,
 };
